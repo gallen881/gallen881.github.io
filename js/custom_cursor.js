e47f5627bc6c1ff81d9d0abf7af646db0c68f475
Gallen Wang <gallen.yes@gmail.com>
2024-12-21 17:56:26 +0800

    Site updated: 2024-12-21 17:56:04

--- js/custom_cursor.js
+++ js/custom_cursor.js
@@ -1,8 +1,12 @@
 const cursor = document.querySelector("#cursor");
 const cursorBorder = document.querySelector("#cursor-border");
 const links = document.querySelectorAll("a");
+const texts = document.querySelectorAll("p");
 const cursorPos = { x: 0, y: 0 };
 const cursorBorderPos = { x: 0, y: 0 };
+
+cursor.style.setProperty("--width", "10px");
+cursor.style.setProperty("--height", "10px");
 
 document.addEventListener("mousemove", (e) => {
   cursorPos.x = e.clientX;
@@ -12,11 +16,39 @@ document.addEventListener("mousemove", (e) => {
 });
 
 document.addEventListener("mousedown", (e) => {
-  cursor.style.setProperty("--size", "20px");
+  cursor.style.setProperty("--width", `${cursor.style.getPropertyValue("--width").replace("px", "") * 1.8}px`);
+  cursor.style.setProperty("--height", `${cursor.style.getPropertyValue("--height").replace("px", "") * 1.8}px`);
 });
 
 document.addEventListener("mouseup", (e) => {
-  cursor.style.setProperty("--size", "8px");
+  cursor.style.setProperty("--width", `${cursor.style.getPropertyValue("--width").replace("px", "") / 1.8}px`);
+  cursor.style.setProperty("--height", `${cursor.style.getPropertyValue("--height").replace("px", "") / 1.8}px`);
+});
+
+texts.forEach((text) => {
+  text.addEventListener("mouseover", (e) => {
+    cursor.style.borderRadius = "0%";
+    cursor.style.setProperty("--width", "2px");
+    cursor.style.setProperty("--height", "20px");
+  });
+  text.addEventListener("mouseout", (e) => {
+    cursor.style.borderRadius = "50%";
+    cursor.style.setProperty("--width", "10px");
+    cursor.style.setProperty("--height", "10px");
+    
+
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", (e) => {
+    cursor.style.backgroundColor = "#daaf61";
+    cursorBorder.style.setProperty("--size", "50px");
+  });
+  link.addEventListener("mouseout", (e) => {
+    cursor.style.backgroundColor = "#95bcd6";
+    cursorBorder.style.setProperty("--size", "27px");
+  });
 });
 
 requestAnimationFrame(function loop() {
